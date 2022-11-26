0957aa4f5fdbedc35a22c320c510229122d160d0
杨顺翔 <yangsx95@qq.com>
2022-11-26 16:54:14 +0800

    feat(clean-tool): 清理Markdown中无用的资源的清理工具初版完成

---
+++ cmd/clean.go
@@ -0,0 +1,177 @@
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/spf13/cobra"
+	"io/fs"
+	"net/url"
+	"os"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+// cleanCmd 清理命令
+var cleanCmd = &cobra.Command{
+	Use:   "clean",
+	Short: "清理本地的markdown文件中的冗余信息",
+	Long:  ``,
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return errors.New("按照规范指定markdown扫描路径与清理路径")
+		}
+		if len(args) > 2 {
+			return errors.New("按照规范指定markdown扫描路径与清理路径")
+		}
+		stat, err := os.Stat(args[0])
+		if os.IsExist(err) {
+			return errors.New("扫描路径不存在")
+		}
+		// 如果目标是个文件 && 文件不是md文件，不处理
+		if !stat.IsDir() && strings.LastIndex(stat.Name(), ".md") < 0 {
+			return errors.New("目标不是以.md结尾的文件，不支持清理")
+		}
+		stat, err = os.Stat(args[1])
+		if os.IsExist(err) {
+			return errors.New("清理路径不存在")
+		}
+		if !stat.IsDir() {
+			return errors.New("清理路径不能是一个文件")
+		}
+		return nil
+	},
+	RunE: func(cmd *cobra.Command, args []string) (err error) {
+		//imgFlag := cmd.Flag("image").Value.String()
+		exerciseFlag := cmd.Flag("exercise").Value.String()
+		stat, _ := os.Stat(args[0])
+		// 从扫描路径中找出所有的md文件，以及非md文件
+		allMdPath := make([]string, 0)
+		allResPath := make([]string, 0)
+		if !stat.IsDir() {
+			allMdPath = append(allMdPath, args[0])
+		} else {
+			mdPath, resPath, err := getAllMdAndResourceFromDir(args[0])
+			if err != nil {
+				return err
+			}
+			allMdPath = append(allMdPath, mdPath...)
+			allResPath = append(allResPath, resPath...)
+		}
+		// 从清理路径中找出所有的md文件以及非md文件
+		mdPath, resPath, err := getAllMdAndResourceFromDir(args[1])
+		if err != nil {
+			return err
+		}
+		allMdPath = append(allMdPath, mdPath...)
+		allResPath = append(allResPath, resPath...)
+
+		// 找出所有的资源文件，不包含md
+		allRefRes := make([]string, 0)
+		// 找出所有的md文件中引用的资源
+		for _, mdPath := range allMdPath {
+			refRes, err := getMdFileResource(mdPath)
+			if err != nil {
+				return err
+			}
+			allRefRes = append(allRefRes, refRes...)
+		}
+
+		// 找出所有未被引用的资源文件
+		allUnRefRes := make([]string, 0)
+		for _, aRes := range allResPath {
+			if !ResourcePathContains(allRefRes, aRes) {
+				allUnRefRes = append(allUnRefRes, aRes)
+			}
+		}
+
+		fmt.Printf("一共查询出%d个Markdown文件, %d个资源(非md)文件, 其中md文件中共引用的资源有%d个，未被引用的资源有%d个\n", len(allMdPath), len(allResPath), len(allRefRes), len(allUnRefRes))
+		fmt.Println("这些资源为：")
+		for _, p := range allUnRefRes {
+			fmt.Println("\t" + p)
+		}
+
+		if exerciseFlag != "true" {
+			fmt.Println("清理中..")
+			for _, unused := range allUnRefRes {
+				_ = os.Remove(unused)
+			}
+			fmt.Println("清理成功...")
+		}
+
+		return err
+	},
+}
+
+func ResourcePathContains(target []string, ele string) bool {
+	for _, t := range target {
+		if strings.TrimSpace(t) == strings.TrimSpace(ele) {
+			return true
+		}
+	}
+	return false
+}
+
+func init() {
+	rootCmd.AddCommand(cleanCmd)
+	cleanCmd.SetUsageTemplate("md-crawler clean <scan_path> <clean_path> [flags]")
+	cleanCmd.Flags().BoolP("exercise", "e", false, "演习清理")
+}
+
+func getMdFileResource(mdPath string) ([]string, error) {
+	file, err := os.ReadFile(mdPath)
+	if err != nil {
+		fmt.Println("读取文件失败" + mdPath + "0--")
+		return nil, err
+	}
+	r := regexp.MustCompile(`\[.*\]\((.+)\)`)
+	resList := r.FindAllStringSubmatch(string(file), -1)
+	result := make([]string, 1)
+	for _, res := range resList {
+		resPath := res[1]
+		// 如果文件链接为url，跳过处理
+		if strings.HasPrefix(resPath, "http") {
+			fmt.Printf("发现url资源 %s %s \n", mdPath, resPath)
+			continue
+		}
+		resPath, _ = url.QueryUnescape(resPath)
+		// 如果文件链接不是绝对路径，将其转换为绝对路径
+		if !filepath.IsAbs(res[1]) {
+			resPath = filepath.Join(filepath.Dir(mdPath), resPath)
+		}
+		result = append(result, resPath)
+	}
+	return result, nil
+}
+
+func getAllMdAndResourceFromDir(dir string) (mdPath []string, resPath []string, err error) {
+	err = filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		path, err = filepath.Abs(path)
+		if err != nil {
+			return err
+		}
+		// 不处理文件夹
+		stat, err := os.Stat(path)
+		if err != nil {
+			return err
+		}
+		if stat.IsDir() {
+			return err
+		}
+		// 不处理.git文件夹下的文件, 以及.gitignore文件
+		if strings.Contains(path, string(os.PathSeparator)+".git"+string(os.PathSeparator)) || stat.Name() == ".gitignore" {
+			return err
+		}
+		info, _ := os.Stat(path)
+		if strings.LastIndex(info.Name(), ".md") > 0 {
+			mdPath = append(mdPath, path)
+		} else {
+			resPath = append(resPath, path)
+		}
+		return err
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+	return mdPath, resPath, err
+}
--- cmd/root.go
+++ cmd/root.go
@@ -1,14 +1,8 @@
 package cmd
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/spf13/cobra"
-	"github.com/spf13/viper"
 )
-
-var cfgFile string
 
 // rootCmd 代表没有子命令的情况下，调用的基本命令
 var rootCmd = &cobra.Command{
@@ -19,42 +13,4 @@ var rootCmd = &cobra.Command{
 
 func Execute() {
 	cobra.CheckErr(rootCmd.Execute())
-}
-
-func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.github.com/yangsx95/md-crawler.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-// initConfig 读取配置文件和环境变量.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := os.UserHomeDir()
-		cobra.CheckErr(err)
-
-		// Search config in home directory with name ".github.com/yangsx95/md-crawler" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigType("yaml")
-		viper.SetConfigName(".github.com/yangsx95/md-crawler")
-	}
-
-	viper.AutomaticEnv() // read in environment variables that match
-
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
-	}
 }
